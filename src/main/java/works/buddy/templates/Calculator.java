f333ad1e46a98e28b6987edf1c5ceb7a9d09911f
Mike Benson <mike@buddy.works>
2015-12-15 15:12:49 +0100

    Add subtracting

--- src/main/java/works/buddy/templates/Calculator.java
+++ src/main/java/works/buddy/templates/Calculator.java
@@ -5,4 +5,9 @@ public class Calculator {
     public int sum(int a, int b) {
         return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a + b;
+    }
+
 }
--- src/test/java/works/buddy/templates/CalculatorTest.java
+++ src/test/java/works/buddy/templates/CalculatorTest.java
@@ -38,4 +38,34 @@ public class CalculatorTest {
     public void testSumTwoMinuses() throws Exception {
         assertEquals(-2, calculator.sum(-1, -1));
     }
+
+    @Test
+    public void testSubtract() throws Exception {
+        assertEquals(0, calculator.subtract(1, 1));
+    }
+
+    @Test
+    public void testSubtractFromZero() throws Exception {
+        assertEquals(-1, calculator.subtract(0, 1));
+    }
+
+    @Test
+    public void testSubtractZero() throws Exception {
+        assertEquals(1, calculator.subtract(1, 0));
+    }
+
+    @Test
+    public void testSubtractZeros() throws Exception {
+        assertEquals(0, calculator.subtract(0, 0));
+    }
+
+    @Test
+    public void testSubtractMinus() throws Exception {
+        assertEquals(1, calculator.subtract(0, -1));
+    }
+
+    @Test
+    public void testSubtractMinuses() throws Exception {
+        assertEquals(0, calculator.subtract(-1, -1));
+    }
 }
