40aa1e9fdd0afc65dad9a835bb65a278601dcea8
tanberryquin <meko.fernandez@gmail.com>
2016-02-25 19:53:05 +0800

    Update CalculatorTest.java
    Test Divisor is negative

--- src/test/java/works/buddy/templates/CalculatorTest.java
+++ src/test/java/works/buddy/templates/CalculatorTest.java
@@ -104,4 +104,9 @@ public class CalculatorTest {
         assertEquals(-1, calculator.divide(-1, 1));
     }        
     
+    @Test
+    public void testDivideNegDivisor() throws Exception {
+        assertEquals(-1, calculator.divide(1, -1));
+    }     
+    
 }
